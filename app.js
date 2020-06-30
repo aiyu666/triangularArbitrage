75ffdc3b92b6638f79f9c335b0c4ef5cff1207e4
aiyu666 <zrobin8741@gmail.com>
2020-07-01 00:15:23 +0800

    Fix wrong calculation

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const request = require('./lib/request');
 let preTime = new Date()
 setInterval(async () => {
     const nowTime = new Date();
-    const btcUSDT = await request.getRequest({ url: 'https://api.bitopro.com/v3/order-book/btc_usdt' })
-    const ethUSDT = await request.getRequest({ url: 'https://api.bitopro.com/v3/order-book/eth_usdt' })
-    const ethBTC = await request.getRequest({ url: 'https://api.bitopro.com/v3/order-book/eth_btc' })
-    if (btcUSDT.statusCode === 200 && ethUSDT.statusCode === 200 && ethBTC.statusCode === 200) {
-        const btcUSDT_asks = JSON.parse(btcUSDT.body).asks[0].price;
-        const btcUSDT_bids = JSON.parse(btcUSDT.body).bids[0].price;
-        const ethUSDT_asks = JSON.parse(ethUSDT.body).asks[0].price;
-        const ethBTC_bids = JSON.parse(ethBTC.body).asks[0].price
-        console.log(`btcUSDT_asks: ${btcUSDT_asks}, btcUSDT_bids: ${btcUSDT_bids}, ethUSDT_asks: ${ethUSDT_asks}, ethBTC_bids: ${ethBTC_bids}`)
-        console.log(`Cost ${ethUSDT_asks} USDT -> 1 ETH, 1 ETH -> ${ethBTC_bids} BTC , ${ethBTC_bids} BTC -> ${btcUSDT_asks * ethBTC_bids} USDT `)
-        console.log(`Make money: ${btcUSDT_asks * ethBTC_bids - ethUSDT_asks}`)
+    const BTC_USDT = await request.getRequest({ url: 'https://api.bitopro.com/v3/order-book/btc_usdt' })
+    const ETH_USDT = await request.getRequest({ url: 'https://api.bitopro.com/v3/order-book/eth_usdt' })
+    const ETH_BTC = await request.getRequest({ url: 'https://api.bitopro.com/v3/order-book/eth_btc' })
+    if (BTC_USDT.statusCode === 200 && ETH_USDT.statusCode === 200 && ETH_BTC.statusCode === 200) {
+        const BTC_USDT_Asks = JSON.parse(BTC_USDT.body).asks[0].price;
+        const ETH_USDT_Bids = JSON.parse(ETH_USDT.body).asks[0].price;
+        const ETH_BTC_Asks = JSON.parse(ETH_BTC.body).asks[0].price
+        console.log(`BTC_USDT_Asks: ${BTC_USDT_Asks}, ETH_USDT_Bids: ${ETH_USDT_Bids}, ETH_BTC_Asks: ${ETH_BTC_Asks}`)
+        console.log(`Cost ${ETH_USDT_Bids} USDT -> 1 ETH -> ${ETH_BTC_Asks} BTC -> ${ETH_BTC_Asks * BTC_USDT_Asks} USDT `)
+        console.log(`Make money: ${ETH_BTC_Asks * BTC_USDT_Asks - ETH_USDT_Bids}USDT, ${((BTC_USDT_Asks * ETH_BTC_Asks - ETH_USDT_Bids) / ETH_USDT_Bids * 100).toFixed(3)}%`)
         console.log(`Spend time: ${nowTime - preTime}`)
         console.log('-----------------------------------')
         preTime = nowTime;
